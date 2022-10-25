182eba640995f5b201750a8c30e88f3e06aa6ba2
BrunaFPereira <brunafpereira15@gmail.com>
2022-10-25 16:58:52 -0300

    codigos var e conversoes

---
+++ conversao.js
@@ -0,0 +1,13 @@
+// Tipo de dado
+// booleanos
+
+
+// Conversão Implícita
+const n = 465;
+const nString = Number("465");
+
+// Number() | String()
+console.log(n+nString)
+
+
+// Conversão Explícita
---
+++ truthy-falsy.js
@@ -0,0 +1,24 @@
+//Boolean
+const teste = true
+const testeOff = false
+
+
+//Truthy ou Falsy
+//0 - false
+//1 - true
+
+console.log(0 == false)
+console.log(1 == true)
+console.log("" == false)
+
+
+//Null | Undefined
+
+// let minhaVar;
+// let varNull = null;
+
+// let num = 3;
+// let tex = "Alura";
+
+// console.log(typeof(minhaVar));
+// console.log(typeof(varNull));
